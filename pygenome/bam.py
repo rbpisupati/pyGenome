8b5f49b0810f95018e369dc92e42f83f67c8a2cd
Rahul Pisupati <rahul.bharadwaj.p@gmail.com>
2025-04-30 13:09:32 +0200

    play with reads at a given position

--- pygenome/bam.py
+++ pygenome/bam.py
@@ -73,6 +73,7 @@ class BamFile(object):
                     continue 
 
             ef_info = ef.to_dict()
+            # randint = str(np.random.choice(1000,1)[0])
             fastq_entry = f"@{ef_info['name']}\n{ef_info['seq']}\n+\n{ef_info['qual']}\n"
             count += 1
             fastqfile.write(fastq_entry)
@@ -80,4 +81,81 @@ class BamFile(object):
         print(f"Printed {count} reads to FASTQ file")
         fastqfile.close()
         
+    def fetch_read_information(self, chr, start, end):
+        """
+        Get read information from BAM file.
+        
+        Args:
+            chr (str): Chromosome name.
+            start (int): Start position.
+            end (int): End position.
+        
+        Returns:
+            list: List of read information dictionaries.
+        """
+        reads = []
+        read_seq = []
+        for ef in self.bam.fetch(chr, start, end):
+            # Skip unmapped reads
+            if ef.is_unmapped:
+                continue
+            # Skip reads with no tags
+            if ef.get_tag("tp") != 'P':
+                print('care, there are non primary aligned reads in the bam file')
+                continue
+            reads.append(ef)
+            read_seq.append( _get_read_seq_from_position(ef, start) )
+            
 
+        read_info = pd.DataFrame([ef.to_dict() for ef in reads])
+        read_info.loc[:,'qry_length'] = [ef.infer_query_length() for ef in reads]
+        read_info.loc[:,'qry_start'] = [ef.query_alignment_start for ef in reads]
+        read_info.loc[:,'qry_end'] = [ef.query_alignment_end for ef in reads]
+        read_info.loc[:,'ref_length'] = [ef.reference_length for ef in reads]
+        read_info.loc[:,'ref_start'] = [ef.reference_start for ef in reads]
+        read_info.loc[:,'ref_end'] = [ef.reference_end for ef in reads]
+        read_info.loc[:,'qry_seq_from_start'] = read_seq
+
+        return({'df':read_info, 'reads': reads})
+
+
+def _get_read_seq_from_position(alignedread, ref_position):
+    """
+    Function to get the sequence from a given position in the reference genome
+    """
+    read_sequence = ''
+    ## first check for the forward reads
+    if alignedread.is_reverse:
+        # Output reverse complement if the read is mapped in reverse
+        # it does not matter if the read is reverse or not
+        # the reference positions and query positions are the same
+        qry_sequence = alignedread.query_sequence
+    else:
+        qry_sequence = alignedread.query_sequence
+    
+    ## read should be aligned until the break point or 5 base pairs before.
+    overlap = alignedread.get_overlap(ref_position - 5, ref_position)
+    # print(overlap)
+    if overlap > 0:
+        ## reads that overlap the breakpoint
+        # now you have two scenarios
+        # 1. reads that pass through the break point
+        # 2. reads that are only mapped till the breakpoint or before 
+        aligned_pairs = alignedread.get_aligned_pairs(matches_only = True)
+        # print((aligned_pairs[0], aligned_pairs[-1]))
+
+        # reads that pass through the break point have last match position higher than the position
+        if aligned_pairs[-1][1] > ref_position:
+            qry_break_ix = [t for t in aligned_pairs if t[1] == ref_position - 1]
+            assert len(qry_break_ix) > 0, 'there must be an aligned position at the break point'
+            # print(qry_break_ix)
+            read_sequence = qry_sequence[qry_break_ix[0][0]:]
+            
+        else: 
+            ## Here the read is aligned either until the breakpoint or before
+            # we need to check if the read is long enough 
+            # if alignedread.infer_query_length() - aligned_pairs[-1][0] > minimum_read_extend_length:
+            ## the read is long enough to extend the breakpoint
+            read_sequence = qry_sequence[aligned_pairs[-1][0]:]
+        
+    return(read_sequence)
