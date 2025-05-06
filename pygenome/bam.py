ccbdaee39487cc7222d57bb1cd470ef5a5136a59
Rahul Pisupati <rahul.bharadwaj.p@gmail.com>
2025-05-06 23:02:04 +0200

    upstream seq

--- pygenome/bam.py
+++ pygenome/bam.py
@@ -94,7 +94,8 @@ class BamFile(object):
             list: List of read information dictionaries.
         """
         reads = []
-        read_seq = []
+        read_seq_down = []
+        read_seq_up = []
         for ef in self.bam.fetch(chr, start, end):
             # Skip unmapped reads
             if ef.is_unmapped:
@@ -104,7 +105,8 @@ class BamFile(object):
                 print('care, there are non primary aligned reads in the bam file')
                 continue
             reads.append(ef)
-            read_seq.append( _get_read_seq_from_position(ef, start) )
+            read_seq_down.append( _get_read_seq_downstream_position(ef, start) )
+            read_seq_up.append( _get_read_seq_upstream_position(ef, end) )
             
 
         read_info = pd.DataFrame([ef.to_dict() for ef in reads])
@@ -114,12 +116,13 @@ class BamFile(object):
         read_info.loc[:,'ref_length'] = [ef.reference_length for ef in reads]
         read_info.loc[:,'ref_start'] = [ef.reference_start for ef in reads]
         read_info.loc[:,'ref_end'] = [ef.reference_end for ef in reads]
-        read_info.loc[:,'qry_seq_from_start'] = read_seq
+        read_info.loc[:,'qry_seq_from_start'] = read_seq_down
+        read_info.loc[:,'qry_seq_before_end'] = read_seq_up
 
         return({'df':read_info, 'reads': reads})
 
 
-def _get_read_seq_from_position(alignedread, ref_position):
+def _get_read_seq_downstream_position(alignedread, ref_position):
     """
     Function to get the sequence from a given position in the reference genome
     """
@@ -157,5 +160,47 @@ def _get_read_seq_from_position(alignedread, ref_position):
             # if alignedread.infer_query_length() - aligned_pairs[-1][0] > minimum_read_extend_length:
             ## the read is long enough to extend the breakpoint
             read_sequence = qry_sequence[aligned_pairs[-1][0]:]
+        
+    return(read_sequence)
+
+
+def _get_read_seq_upstream_position(alignedread, ref_position):
+    """
+    Function to get the sequence from a given position in the reference genome
+    """
+    read_sequence = ''
+    ## first check for the forward reads
+    if alignedread.is_reverse:
+        # Output reverse complement if the read is mapped in reverse
+        # it does not matter if the read is reverse or not
+        # the reference positions and query positions are the same
+        qry_sequence = alignedread.query_sequence
+    else:
+        qry_sequence = alignedread.query_sequence
+    
+    ## read should be aligned until the break point or 5 base pairs after.
+    overlap = alignedread.get_overlap(ref_position, ref_position + 5)
+    # print(overlap)
+    if overlap > 0:
+        ## reads that overlap the breakpoint
+        # now you have two scenarios
+        # 1. reads that pass through the break point
+        # 2. reads that are only mapped till the breakpoint or before 
+        aligned_pairs = alignedread.get_aligned_pairs(matches_only = True)
+        # print((aligned_pairs[0], aligned_pairs[-1]))
+
+        # reads that pass through the break point have match position before ref_position
+        if aligned_pairs[0][1] < ref_position:
+            qry_break_ix = [t for t in aligned_pairs if t[1] == ref_position - 1]
+            assert len(qry_break_ix) > 0, 'there must be an aligned position at the break point'
+            # print(qry_break_ix)
+            read_sequence = qry_sequence[:qry_break_ix[0][0]]
+            
+        else: 
+            ## Here the read is aligned either until the breakpoint or before
+            # we need to check if the read is long enough 
+            # if alignedread.infer_query_length() - aligned_pairs[-1][0] > minimum_read_extend_length:
+            ## the read is long enough to extend the breakpoint
+            read_sequence = qry_sequence[:aligned_pairs[-1][0]]
         
     return(read_sequence)
